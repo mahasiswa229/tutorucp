4539c725667e640fb159ebd5d07649753abefbb2
mahasiswa229 <161399711+mahasiswa229@users.noreply.github.com>
2024-03-20 23:11:54 +0800

    algoritma sortir nich

--- rawr.cpp
+++ rawr.cpp
@@ -1,8 +1,26 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main()
+int arr[20];
+int n;
+
+void ilamti()
 {
- cout << "hello dunia" << endl;
- 
+    int pass=1;
+    do
+    {
+        for (int i = 0; i <= n - 1- pass ; i++)
+        {if (arr[i] > arr[i + 1])
+        {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp; 
+        }  
+        }
+        pass = pass + 1;
+    }   while (pass <= n - 1);
+}
+
+int main(){
+
 }
